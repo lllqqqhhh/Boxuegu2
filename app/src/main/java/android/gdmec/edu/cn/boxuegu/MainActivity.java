7d53206e1a7ee5db29dce292d1ca9a3373625a7e
lllqqqhhh <565743123@qq.com>
2017-12-26 08:47:37 +0800

    Initial commit

---
+++ app/src/main/java/android/gdmec/edu/cn/boxuegu/MainActivity.java
@@ -0,0 +1,13 @@
+package android.gdmec.edu.cn.boxuegu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
