3c95398d67b42269ed9b4977322f460f00b22d2d
lllqqqhhh <565743123@qq.com>
2017-12-28 11:17:09 +0800

    林秋虹 2017.12.28 01

--- app/src/main/java/android/gdmec/edu/cn/boxuegu/adapter/AdBannerAdapter.java
+++ app/src/main/java/android/gdmec/edu/cn/boxuegu/adapter/AdBannerAdapter.java
@@ -1,8 +1,63 @@
 package android.gdmec.edu.cn.boxuegu.adapter;
 
+import android.gdmec.edu.cn.boxuegu.bean.CourseBean;
+import android.gdmec.edu.cn.boxuegu.fragment.AdBeannerFragment;
+import android.gdmec.edu.cn.boxuegu.view.CourseView;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by student on 17/12/27.
  */
 
-public class AdBannerAdapter {
+public class AdBannerAdapter extends FragmentStatePagerAdapter implements View.OnTouchListener {
+    private Handler mHandler;
+    private List<CourseBean> cabl;
+    public AdBannerAdapter(FragmentManager fm){
+        super(fm);
+        cabl = new ArrayList<CourseBean>();
+    }
+    public AdBannerAdapter(FragmentManager fm,Handler handler){
+        super(fm);
+        mHandler = handler;
+        cabl = new ArrayList<CourseBean>();
+    }
+    public void setDatas(List<CourseBean> cabl){
+        this.cabl = cabl;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+        Bundle args = new Bundle();
+        if (cabl.size()>0)
+            args.putString("ad",cabl.get(index%cabl.size()).icon);
+        return AdBeannerFragment.newInstance(args);
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+    public int getSize(){
+        return cabl == null?0:cabl.size();
+    }
+    @Override
+    public int getItemPosition(Object object){
+        return POSITION_NONE;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        mHandler.removeMessages(CourseView.MSG_AD_SLID);
+        return false;
+    }
 }
--- app/src/main/java/android/gdmec/edu/cn/boxuegu/adapter/PlayHistoryAdapter.java
+++ app/src/main/java/android/gdmec/edu/cn/boxuegu/adapter/PlayHistoryAdapter.java
@@ -1,8 +1,115 @@
 package android.gdmec.edu.cn.boxuegu.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.gdmec.edu.cn.boxuegu.R;
+import android.gdmec.edu.cn.boxuegu.activity.VideoPlayActivity;
+import android.gdmec.edu.cn.boxuegu.bean.VideoBean;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by student on 17/12/27.
  */
 
-public class PlayHistoryAdapter {
+public class PlayHistoryAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<VideoBean> vbl;
+    public PlayHistoryAdapter(Context context){
+        this.mContext = context;
+    }
+    public void setData(List<VideoBean> vbl){
+        this.vbl = vbl;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return vbl == null ? 0 : vbl.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return vbl == null ? null :vbl.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder vh;
+        if (convertView == null){
+            vh = new ViewHolder();
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.play_history_list_item,null);
+            vh.tv_title = (TextView) convertView.findViewById(R.id.tv_adapter_title);
+            vh.tv_video_title = (TextView) convertView.findViewById(R.id.tv_video_title);
+            vh.iv_icon = (ImageView) convertView.findViewById(R.id.iv_video_icon);
+            convertView.setTag(vh);
+        }else {
+            vh = (ViewHolder)convertView.getTag();
+        }
+        final VideoBean bean = (VideoBean) getItem(position);
+        if (bean != null){
+            vh.tv_title.setText(bean.title);
+            vh.tv_video_title.setText(bean.secondTitle);
+            switch (bean.chapterId){
+                case 1:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon1);
+                    break;
+                case 2:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon2);
+                    break;
+                case 3:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon3);
+                    break;
+                case 4:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon4);
+                    break;
+                case 5:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon5);
+                    break;
+                case 6:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon6);
+                    break;
+                case 7:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon7);
+                    break;
+                case 8:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon8);
+                    break;
+                case 9:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon9);
+                    break;
+                case 10:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon10);
+                    break;
+                default:
+                    vh.iv_icon.setImageResource(R.drawable.video_play_icon1);
+                    break;
+            }
+        }
+        convertView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (bean == null)return;
+                Intent intent = new Intent(mContext, VideoPlayActivity.class);
+                intent.putExtra("videoPath",bean.videoId);
+                mContext.startActivity(intent);
+            }
+        });
+        return convertView;
+    }
+    class ViewHolder{
+        public TextView tv_title,tv_video_title;
+        public ImageView iv_icon;
+    }
 }
